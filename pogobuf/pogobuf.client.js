1377a55c680353c51f313e4a6553e6f638683927
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2017-10-05 20:25:58 +0200

    update lib and handle 7501

--- pogobuf/pogobuf.client.js
+++ pogobuf/pogobuf.client.js
@@ -683,7 +683,8 @@ function Client(options) {
             self.hashingVersion = self.options.hashingVersion;
             return Promise.resolve();
         } else {
-            const version = self.options.version;
+            const version = +self.options.version;
+            if (version === 7501) version = 7500;
             return Signature.versions.getHashingEndpoint(self.options.hashingServer, version)
                 .then(hashVersion => {
                     self.hashingVersion = hashVersion;
